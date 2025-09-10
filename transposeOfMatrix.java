a377d648a0ca599c333b30e7e2a2b493f185e6a7
prince8789 <princekumarbgu9097@gmail.com>
2025-09-10 17:20:46 +0530

    in place transpose

--- transposeOfMatrix.java
+++ transposeOfMatrix.java
@@ -10,25 +10,43 @@ public class transposeOfMatrix {
         System.out.println("enter the number of column ");
         int col = sc.nextInt();
         int [][]arr = new int[row][col];
-        System.out.println("enter the "+row*col+"elements");
+        System.out.println("enter the "+row*col+" elements");
         for(int i = 0 ;i< row; i++){
             for(int j =0 ; j<col; j++){
                 arr[i][j]=sc.nextInt();
             }
         }
-        int [][]ans=Transpose(arr,row,col);
+        transposewithoutextraSpace(arr, row, col);
+        //int [][]ans=Transpose(arr,row,col);
         System.out.println("transpose of matrix is ");
-        print2dArray(ans);
+        print2dArray(arr);
 
     }
-    static int[][] Transpose(int [][]arr,int row,int col){
-        int [][] ans = new int[col][row];
-        for(int i =0;i<col;i++){
-            for(int j =0;j<row;j++){
-                ans[i][j]=arr[j][i];
+    // static int[][] Transpose(int [][]arr,int row,int col){
+    //     int [][] ans = new int[col][row];
+    //     for(int i =0;i<col;i++){
+    //         for(int j =0;j<row;j++){
+    //             ans[i][j]=arr[j][i];
+    //         }
+    //     }
+    //     return ans;
+    // }
+    static void transposewithoutextraSpace(int [][]arr, int row,int col){
+      if(row!=col){
+        System.out.println("enter the square matrix");
+        return;
+      }
+      else{
+          for(int i =0;i<row;i++){
+            for(int j =i+1;j<col;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
             }
+
         }
-        return ans;
+      }
+
     }
   
      static void print2dArray(int [][]arr){
