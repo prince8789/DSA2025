0839b9c40b12bdd879f68be0a677995fcf866240
prince kumar <princekumarbgu8789@gmail.com>
2025-10-01 19:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (12.11%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int empty_bottle = numBottles;
+        int count = numBottles;
+        while(empty_bottle>=numExchange){
+            int newbottle  =empty_bottle/numExchange;
+            count =count +newbottle ;
+            
+            empty_bottle = empty_bottle % numExchange + newbottle;
+            //empty_bottle = empty_bottle%numExchange;
+
+        }
+        return count;
+    }
+}
