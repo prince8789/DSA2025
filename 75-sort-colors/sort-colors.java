507877955190e692987f35dbf6009f24dc57d1c5
prince kumar <princekumarbgu8789@gmail.com>
2025-10-09 12:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (67.89%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero =0;
+        int one =0;
+        int two =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==0){
+                zero++;
+
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else if(nums[i]==2){
+                two++;
+            }
+        }
+        for(int i =0;i<nums.length;i++){
+            if(zero>0){
+                nums[i]=0;
+                zero--;
+            }
+            else if(one>0){
+                nums[i]=1;
+                one--;
+            }
+            else if(two>0){
+                nums[i]=2;
+                two--;
+            }
+        }
+        
+    }
+}
