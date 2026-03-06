0cc7e950072eba3b09ee36ce90421a0da3b7b8a5
prince kumar <princekumarbgu8789@gmail.com>
2026-03-06 12:08:26 +0530

    Time: 2 ms (23.04%), Space: 43.7 MB (99.97%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -1,15 +1,17 @@
 class Solution {
     public String trimTrailingVowels(String s) {
         StringBuilder sb = new StringBuilder(s);
-        for(int i = sb.length()-1;i>=0;i--){
+
+        for (int i = sb.length() - 1; i >= 0; i--) {
             char ch = sb.charAt(i);
-            if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u'){
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 sb.deleteCharAt(i);
-            }
-            else{
-                return sb.toString();
+            } else {
+                break;
             }
         }
+
         return sb.toString();
     }
 }
