ea7acefb2e0c95a909726cdd5c45f317d2dece19
prince kumar <princekumarbgu8789@gmail.com>
2026-04-17 21:23:13 +0530

    Time: 2 ms (87.63%), Space: 49.3 MB (40.47%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null){
+            return;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        ListNode second = reverseList(slow.next);
+        slow.next = null;
+
+        ListNode first = head;
+
+        while(second != null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
