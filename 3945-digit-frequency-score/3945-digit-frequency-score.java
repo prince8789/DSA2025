6c3efd34e8265bfbb569f845307c01ac067883e8
prince kumar <princekumarbgu8789@gmail.com>
2026-05-31 22:54:25 +0530

    Time: 1 ms (99.49%), Space: 42.4 MB (90.43%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,9 +1,9 @@
 class Solution {
     public int digitFrequencyScore(int n) {
         int[] arr = new int[10];
-        for(int i =0;i<arr.length;i++){
-            arr[i]=-1;
-        }
+        // for(int i =0;i<arr.length;i++){
+        //     arr[i]=-1;
+        // }
         while(n>0){
             int num = n%10;
             arr[num]++;
@@ -11,8 +11,8 @@ class Solution {
         }
         int sum =0;
         for(int i =0;i<arr.length;i++){
-            if(arr[i]>-1){
-                sum = sum+i*(++arr[i]);
+            if(arr[i]>0){
+                sum = sum+i*arr[i];
             }
         }
         return sum;
