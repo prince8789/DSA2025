bed37a021e7a82906be3c3b479500b3713f0dfb4
prince kumar <princekumarbgu8789@gmail.com>
2026-06-23 07:25:01 +0530

    Time: 2 ms (70.31%), Space: 47.1 MB (72.16%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,13 +1,35 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<nums.length;i++){
-            int curr = 1;
-            for(int j =i;j<nums.length;j++){
-                curr *=nums[j];
-                max = Math.max(max,curr);
+
+        // Maximum product ending at current index
+        int maxProd = nums[0];
+
+        // Minimum product ending at current index
+        int minProd = nums[0];
+
+        // Final answer
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            // If current number is negative,
+            // maximum and minimum swap roles
+            if (nums[i] < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
             }
+
+            // Either start fresh from nums[i]
+            // or extend previous product
+            maxProd = Math.max(nums[i], maxProd * nums[i]);
+
+            minProd = Math.min(nums[i], minProd * nums[i]);
+
+            // Update global maximum
+            ans = Math.max(ans, maxProd);
         }
-        return max;
+
+        return ans;
     }
 }
