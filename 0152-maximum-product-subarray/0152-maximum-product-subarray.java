baaf4f8d95d0655d6b2a4c7b3ab86e889973abec
prince kumar <princekumarbgu8789@gmail.com>
2026-06-23 07:16:29 +0530

    Time: 269 ms (10.16%), Space: 47.1 MB (72.16%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<nums.length;i++){
+            int curr = 1;
+            for(int j =i;j<nums.length;j++){
+                curr *=nums[j];
+                max = Math.max(max,curr);
+            }
+        }
+        return max;
+    }
+}
