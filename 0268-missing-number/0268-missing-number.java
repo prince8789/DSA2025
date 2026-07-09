c9f68463abb90ad489d80bd45c5f0a0dd45d2581
prince kumar <princekumarbgu8789@gmail.com>
2026-07-09 06:25:00 +0530

    Time: 0 ms (100%), Space: 47.1 MB (91.63%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum =0;
+        int n_sum = (n*(n+1))/2;
+        for(int i =0;i<n;i++){
+            sum = sum+nums[i];
+        }
+        return n_sum-sum;
+    }
+
+}
