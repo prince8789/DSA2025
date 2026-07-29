bc145bfa77a261eb49ea2047345b2919a779d59e
prince kumar <princekumarbgu8789@gmail.com>
2026-07-29 12:08:09 +0530

    Time: 6 ms (43.26%), Space: 44.1 MB (90.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,10 +7,8 @@ class Solution {
         for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
             arr[ch-'a']++;
-        }
-        for(int i =0;i<t.length();i++){
-            char ch = t.charAt(i);
-            arr[ch-'a']--;
+            char ch1 = t.charAt(i);
+            arr[ch1 -'a']--;
         }
         for(int i =0;i<26;i++){
             if(arr[i] != 0){
